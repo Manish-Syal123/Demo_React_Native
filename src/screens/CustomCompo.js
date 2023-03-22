cda23ae591684ab1d646ce8b3a4ea4e7c76922eb
Manish_Syal <manishsyal77@gmail.com>
2023-03-22 20:10:17 +0530

    Learning same jsx rules as in React.js

--- src/screens/CustomCompo.js
+++ src/screens/CustomCompo.js
@@ -2,9 +2,15 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 const CustomCompo = () => {
+  const myname = "Manish";
+  const myElement = <Text style={styles.textStyle}>My name is {myname}</Text>;
+  const getFullName = (a, b, c) => {
+    return `Hi mera name ${a} ${b} ${c}`;
+  };
   return (
     <View style={styles.viewstyle}>
-      <Text style={styles.textStyle}>Hello World!</Text>
+      {myElement}
+      <Text>Guess my name is {getFullName("Manish", "BK", "Syal")}</Text>
     </View>
   );
 };
@@ -13,12 +19,13 @@ const styles = StyleSheet.create({
   textStyle: {
     color: "red",
     // backgroundColor: "black",
+    marginTop: 200,
   },
-  viewstyle: {
-    backgroundColor: "green",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  // viewstyle: {
+  //   backgroundColor: "green",
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
 });
 
 export default CustomCompo;
