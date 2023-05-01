8dd702175c32400bdb386a49be681ea5ee4d6772
Manish_Syal <manishsyal77@gmail.com>
2023-05-01 16:50:11 +0530

    If else condition in flatlist with creating function

--- src/screens/FlatListDemo.js
+++ src/screens/FlatListDemo.js
@@ -3,25 +3,34 @@ import { Text, View, StyleSheet, FlatList } from "react-native";
 import namedata from "./arraydata";
 
 const FlatListDemo = () => {
+  const callname = (elem) => {
+    if (elem.item.name === "Hacker") {
+      return elem.item.work + " Manish";
+    }
+  };
   const names = [
     {
       name: "Manish",
+      work: "computer",
     },
     {
       name: "BK",
+      work: "computer",
     },
     {
       name: "Syal",
+      work: "computer",
     },
     {
       name: "Hacker",
+      work: "Cyber security",
     },
   ];
   return (
     <FlatList
       data={names}
       renderItem={(elem) => {
-        return <Text style={styles.textStyle}>{elem.item.name}</Text>;
+        return <Text style={styles.textStyle}>{callname(elem)}</Text>;
       }}
     />
   );
